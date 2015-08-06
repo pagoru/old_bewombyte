3869a09cc428e33330f295f09d63a08a30419576
pagoru <pagoru@gmail.com>
2015-08-06 02:24:04 +0200

    Logs

--- bewomByte/src/main/java/es/bewom/commands/CommandSay.java
+++ bewomByte/src/main/java/es/bewom/commands/CommandSay.java
@@ -1,11 +1,9 @@
 package es.bewom.commands;
 
-import java.util.Collection;
-
 import org.spongepowered.api.Game;
 import org.spongepowered.api.entity.player.Player;
+import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.Texts;
-import org.spongepowered.api.text.chat.ChatTypes;
 import org.spongepowered.api.text.format.TextColors;
 import org.spongepowered.api.text.format.TextStyles;
 import org.spongepowered.api.util.command.CommandException;
@@ -14,9 +12,9 @@ import org.spongepowered.api.util.command.CommandSource;
 import org.spongepowered.api.util.command.args.CommandContext;
 import org.spongepowered.api.util.command.spec.CommandExecutor;
 
+import es.bewom.chat.Chat;
 import es.bewom.texts.TextMessages;
 import es.bewom.user.BewomUser;
-import es.bewom.user.WebRegistration;
 
 public class CommandSay implements CommandExecutor {
 	
@@ -34,14 +32,9 @@ public class CommandSay implements CommandExecutor {
 			BewomUser user = BewomUser.getUser((Player) src);
 			if(src.hasPermission("any") || user.getPermissionLevel() >= BewomUser.PERM_LEVEL_ADMIN) {
 				String message = args.<String>getOne("mensaje").get();
-				Collection<Player> players = game.getServer().getOnlinePlayers();
-				for(Player player : players) {
-					if(BewomUser.getUser(player).getRegistration() == WebRegistration.VALID){
-						player.sendMessage(ChatTypes.SYSTEM, Texts.of(
-								TextMessages.BROADCAST,
-								TextStyles.BOLD, TextColors.DARK_RED, message));
-					}
-				}
+				Text nm = Texts.of(TextMessages.BROADCAST, TextStyles.BOLD, TextColors.DARK_RED, message);
+				
+				Chat.sendMessage((Player) src, "/say " + message, nm);
 				return CommandResult.success();
 			}
 		}
